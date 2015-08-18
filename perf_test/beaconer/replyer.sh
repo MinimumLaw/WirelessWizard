78b714705976538203aa46af94914f9544beee08
Alex A. Mihaylov <minimumlaw@rambler.ru>
2015-08-18 16:04:44 +0300

    Add debug script for beaconer sender and replyer

---
+++ perf_test/beaconer/replyer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PANID=1234
+SRC=2
+DST=1
+
+NETDEV=wpan0
+CHAN=0
+MAC=cafebabedeadbeaf
+
+iz add ${1} ${NETDEV} ${MAC}
+iz set ${NETDEV} ${PANID} ${SRC} ${CHAN}
+ifconfig ${NETDEV} up
+
+
+./beaconer ${PANID} ${SRC} ${DST}
